5facdf2c629d0a0064493677f3d8d3a5079b24d6
Lorenzo Pini <lore.pini@tin.it>
2016-04-01 14:06:31 +0200

    Add CC0 licence on footer

--- frontend/static/config/vibi.js
+++ frontend/static/config/vibi.js
@@ -23,6 +23,20 @@
 			"height": 86
 	   }
     },
+    "footer":{
+        "html": "<p xmlns:dct=\"http://purl.org/dc/terms/\" xmlns:vcard=\"http://www.w3.org/2001/vcard-rdf/3.0#\"> <a rel=\"license\" href=\"http://creativecommons.org/publicdomain/zero/1.0/\"><img src=\"https://licensebuttons.net/p/zero/1.0/88x31.png\" style=\"border-style: none; vertical-align: middle;\" alt=\"CC0\" /></a>To the extent possible under law,  <a rel=\"dct:publisher\" href=\"http://www.clevelandmetroparks.com\"><span property=\"dct:title\">Cleveland Metroparks</span></a> has waived all copyright and related or neighboring rights to this work. This work is published from:<span property=\"vcard:Country\" datatype=\"dct:ISO3166\" content=\"US\" about=\"http://www.clevelandmetroparks.com\">United States</span>.</p>",
+        "container": {
+            "border": false,
+            "header": false,
+            "collapsible": false,
+            "hideCollapseTool": true,
+            "split": true,
+            "animCollapse": false,
+            "minHeight": 32,
+            "maxHeight": 32,
+            "height": 32
+        }
+    },
     "ASSET": {
         "delete_icon": "./externals/mapmanager/theme/img/user_delete.png",
         "edit_icon": "./externals/mapmanager/theme/img/user_edit.png"
