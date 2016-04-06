7e2bf30b77a0e1a8313fdafa1a0ea8f3e65a7b95
Lorenzo Pini <lore.pini@tin.it>
2016-04-06 15:13:12 +0200

    Open licence in another tab

--- frontend/static/config/vibi.js
+++ frontend/static/config/vibi.js
@@ -24,7 +24,7 @@
 	   }
     },
     "footer":{
-        "html": "<p xmlns:dct=\"http://purl.org/dc/terms/\" xmlns:vcard=\"http://www.w3.org/2001/vcard-rdf/3.0#\"> <a rel=\"license\" href=\"http://creativecommons.org/publicdomain/zero/1.0/\"><img src=\"https://licensebuttons.net/p/zero/1.0/88x31.png\" style=\"border-style: none; vertical-align: middle;\" alt=\"CC0\" /></a>To the extent possible under law,  <a rel=\"dct:publisher\" href=\"http://www.clevelandmetroparks.com\"><span property=\"dct:title\">Cleveland Metroparks</span></a> has waived all copyright and related or neighboring rights to this work. This work is published from:<span property=\"vcard:Country\" datatype=\"dct:ISO3166\" content=\"US\" about=\"http://www.clevelandmetroparks.com\">United States</span>.</p>",
+        "html": "<p xmlns:dct=\"http://purl.org/dc/terms/\" xmlns:vcard=\"http://www.w3.org/2001/vcard-rdf/3.0#\"> <a rel=\"license\" href=\"http://creativecommons.org/publicdomain/zero/1.0/\" target=\"_blank\"><img src=\"https://licensebuttons.net/p/zero/1.0/88x31.png\" style=\"border-style: none; vertical-align: middle;\" alt=\"CC0\" /></a>To the extent possible under law,  <a rel=\"dct:publisher\" href=\"http://www.clevelandmetroparks.com\"><span property=\"dct:title\">Cleveland Metroparks</span></a> has waived all copyright and related or neighboring rights to this work. This work is published from:<span property=\"vcard:Country\" datatype=\"dct:ISO3166\" content=\"US\" about=\"http://www.clevelandmetroparks.com\">United States</span>.</p>",
         "container": {
             "border": false,
             "header": false,
